9e6393d93eaa5a0b4a37c29af2dfdf5229e24663
PascualHache <hector.pascual.diaz@gmail.com>
2023-12-15 13:00:15 +0100

    [feature/seo-improvements]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import TopBar from "./components/top-bar/TopBar";
+import { HelmetProvider } from "react-helmet-async";
 import Main from "./views/Main";
 import Proyectos from "./views/Proyectos";
 import Talentos from "./views/Talentos";
-import "./fonts.css";
 import styled from "styled-components";
 import greenSand from "./assets/graphics/green-sand.svg";
+import "./fonts.css";
+import SEO from "./views/components/SEO";
 
 const StyledApp = styled.div`
   font-family: RobotoCondensed;
@@ -12,13 +14,22 @@ const StyledApp = styled.div`
   background-repeat: repeat;
 `;
 function App() {
+  const helmetContext = {};
   return (
-    <StyledApp>
-      <TopBar />
-      <Main />
-      <Talentos />
-      <Proyectos />
-    </StyledApp>
+    <HelmetProvider context={helmetContext}>
+      <SEO
+        title="UBIQ"
+        description="Somos una agencia de publicidad, creación de contenido y representación de talento. Representamos a TheGrefg, Gemita, Gonsabella, Llunaclark, Mixwell, Rogercarbo, Paaulacg. Organizamos eventos como MiniTitans, Esland o Ping Pong Challenge."
+        name="UBIQ"
+        type="landing"
+      />
+      <StyledApp>
+        <TopBar />
+        <Main />
+        <Talentos />
+        <Proyectos />
+      </StyledApp>
+    </HelmetProvider>
   );
 }
 
